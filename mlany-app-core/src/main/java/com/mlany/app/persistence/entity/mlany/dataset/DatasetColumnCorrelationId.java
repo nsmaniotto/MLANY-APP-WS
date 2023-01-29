8bf9fe8f6daeac559332368456752d8f5ae62db2
Nathan Smaniotto <smaniotto.nathan@gmail.com>
2023-01-29 12:36:48 +0100

    Added Equals and Hashcode override for DatasetColumnCorrelationId class

--- mlany-app-core/src/main/java/com/mlany/app/persistence/entity/mlany/dataset/DatasetColumnCorrelationId.java
+++ mlany-app-core/src/main/java/com/mlany/app/persistence/entity/mlany/dataset/DatasetColumnCorrelationId.java
@@ -3,10 +3,12 @@ package com.mlany.app.persistence.entity.mlany.dataset;
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = { "firstColumn", "secondColumn" })
 public class DatasetColumnCorrelationId implements Serializable {
 
 	private static final long serialVersionUID = 1L;
